0316534fbf9e88bbf88e50b4ed75fed40530e58c
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-10-03 19:44:29 +0200

    Set datepickers start day to monday, time interval, and disable weekends

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [date, setDate] = useState(new Date());
+    const pastDates = (date) => new Date() < date;
+
+    return (
+        <div>
+            <DatePicker
+                showTimeSelect
+                minTime={new Date(0, 0, 0, 9, 0)}
+                maxTime={new Date(0, 0, 0, 17, 0)}
+                filterDate={date => {
+                    // Disable weekends (Saturday and Sunday)
+                    return new Date() < date && date.getDay() !== 0 && date.getDay() !== 6;
+                }}
+                selected={date}
+                onChange={(date) => setDate(date)}
+                dateFormat="MMMM d, yyyy h:mmaa"
+                calendarStartDay={1}
+            />
+        </div>
+    );
 }
-
-export default App
