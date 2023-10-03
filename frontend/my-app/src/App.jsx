49171eb0077ad4d74b2dddab26c60c315d59ab23
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2023-10-03 20:51:38 +0200

    Add filtered time slots

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -4,8 +4,21 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 export default function App() {
 
-    const [date, setDate] = useState(new Date());
-    const pastDates = (date) => new Date() < date;
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    console.log(selectedDate);
+
+    const minTime = new Date();
+    minTime.setHours(9, 0, 0);
+    const maxTime = new Date();
+    maxTime.setHours(17, 0, 0);
+
+    const filterWeekends = (date) => {
+        return date.getDay() !== 0 && date.getDay() !== 6;
+    };
+
+    const filterPastDates = (date) => {
+        return new Date() < date;
+    }
 
     return (
         <div>
@@ -14,13 +27,13 @@ export default function App() {
                 minTime={new Date(0, 0, 0, 9, 0)}
                 maxTime={new Date(0, 0, 0, 17, 0)}
                 filterDate={date => {
-                    // Disable weekends (Saturday and Sunday)
-                    return new Date() < date && date.getDay() !== 0 && date.getDay() !== 6;
+                    return filterPastDates(date) && filterWeekends(date);
                 }}
-                selected={date}
-                onChange={(date) => setDate(date)}
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
                 dateFormat="MMMM d, yyyy h:mmaa"
                 calendarStartDay={1}
+                inline
             />
         </div>
     );
