124d8932069643a82d4285d2bd1c34fc74b5c49d
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-01-19 11:34:24 +0100

    Create Button component with bootstrap

---
+++ frontend/my-app/src/Components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({ text, onClick, type = 'primary' }) => {
+    return (
+        <button
+            className={`btn btn-${type}`}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
+};
+
+export default Button;
