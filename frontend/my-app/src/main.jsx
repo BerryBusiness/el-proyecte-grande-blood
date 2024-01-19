8c9fff0f9b337fcc9bb24716407cfc56c05bf8aa
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-01-19 11:36:47 +0100

    Add route of Contact component

--- frontend/my-app/src/main.jsx
+++ frontend/my-app/src/main.jsx
@@ -9,6 +9,9 @@ import RegisterPage from "./Pages/RegisterPage.jsx";
 import UpdatePage from "./Pages/UpdatePage.jsx";
 import AdminPage from "./Pages/AdminPage.jsx";
 import {UserProvider} from "./Pages/UserContext.jsx";
+import ContactPage from "./Pages/ContactPage.jsx";
+
+
 
 const router = createBrowserRouter([
     {
@@ -41,6 +44,10 @@ const router = createBrowserRouter([
             {
                 path: "/admin",
                 element: <AdminPage/>,
+            },
+            {
+                path: "/contact",
+                element: <ContactPage/>,
             },
         ],
     },
