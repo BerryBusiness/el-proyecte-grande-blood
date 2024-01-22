c4149ea0574b7526b8fd9603a6ed771e0abeb60c
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-01-22 13:08:02 +0100

    Create Package repository if

---
+++ backend/src/main/java/com/codecool/imf/repository/PackageRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.imf.repository;
+
+import com.codecool.imf.model.Package;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PackageRepository extends JpaRepository<Package, Long> {
+}
+
