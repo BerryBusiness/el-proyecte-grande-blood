f2cc6d11f11bd4693823bfa6c11e3398cab2a130
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-01-29 14:19:28 +0100

    Create sql file for initializing admins in db

---
+++ backend/src/main/resources/adminInit.sql
@@ -0,0 +1,7 @@
+-- Add Admins
+INSERT INTO USERS (EMAIL, NAME, PASSWORD, ROLE)
+VALUES ('milan@imf.com', 'Milan', 1234, 'ADMIN');
+INSERT INTO USERS (EMAIL, NAME, PASSWORD, ROLE)
+VALUES ('mark@imf.com', 'Mark', 1234, 'ADMIN');
+INSERT INTO USERS (EMAIL, NAME, PASSWORD, ROLE)
+VALUES ('malnin@imf.com', 'Malni', 1234, 'ADMIN');
