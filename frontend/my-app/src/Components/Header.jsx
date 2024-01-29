114ed5e32eadd2faa271b6750cfaab7789be50bc
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-01-29 14:21:37 +0100

    Add buttons to header component

--- frontend/my-app/src/Components/Header.jsx
+++ frontend/my-app/src/Components/Header.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import UserContext from "../Pages/UserContext.jsx";
 import LinkButton from "./LinkButton.jsx";
 
-const Header = ({ handleClick }) => {
+const Header = () => {
     const { user, setUser } = React.useContext(UserContext);
 
     const handleContactClick = () => {
-        console.log('Button clicked!');
+        console.log('Contact Button clicked!');
+    };
+    const handlePackagesClick = () => {
+        console.log('Pack Button clicked!');
+    };
+    const handleMainClick = () => {
+        console.log('Main Button clicked!');
     };
 
     return (
@@ -15,7 +21,11 @@ const Header = ({ handleClick }) => {
                 <img src="/imf_logo.png" alt="IMF logo" className="ImfLogo"/>
                 {user ? `Welcome ${user.name} !` : "IMF REDLABS LAB SERVICES"}
             </div>
+            <div className="buttonContainer">
             <LinkButton to="/contact" text="Contact" onClick={handleContactClick}/>
+            <LinkButton to="/packages" text="Packages" onClick={handlePackagesClick}/>
+            <LinkButton to="/" text="Main Page" onClick={handleMainClick}/>
+            </div>
         </header>
     );
 };
