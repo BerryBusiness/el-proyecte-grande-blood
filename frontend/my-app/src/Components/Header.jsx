428997e17fbcd300e26f37676e51bf1834d78bf3
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-01-30 13:35:18 +0100

    Add buttons and refactor rendering

--- frontend/my-app/src/Components/Header.jsx
+++ frontend/my-app/src/Components/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import UserContext from "../Pages/UserContext.jsx";
 import LinkButton from "./LinkButton.jsx";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 
-const Header = () => {
+const Header = ({ isAdmin, appointment, id }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const currentPath = location.pathname;
+
     const { user, setUser } = React.useContext(UserContext);
 
     const handleContactClick = () => {
@@ -15,16 +20,49 @@ const Header = () => {
         console.log('Main Button clicked!');
     };
 
+
+    const handleLogoutClick = () => {
+        localStorage.removeItem('jwtToken');
+        setUser(null);
+        navigate('/login');
+    };
+
     return (
         <header>
             <div className="mainTitle">
-                <img src="/imf_logo.png" alt="IMF logo" className="ImfLogo"/>
-                {user ? `Welcome ${user.name} !` : "IMF REDLABS LAB SERVICES"}
+                <Link to="/">
+                    <img src="/imf_logo.png" alt="IMF logo" className="ImfLogo"/>
+                </Link>
+                {user ? `Welcome ${user.name}` : "IMF REDLABS LAB SERVICES"}
+
             </div>
             <div className="buttonContainer">
-            <LinkButton to="/contact" text="Contact" onClick={handleContactClick}/>
-            <LinkButton to="/packages" text="Packages" onClick={handlePackagesClick}/>
-            <LinkButton to="/" text="Main Page" onClick={handleMainClick}/>
+                {user ? (
+                    <>
+                        {currentPath !== `/update/${id}` && (
+                            <LinkButton to={`/update/${id}`} text="Update Account" onClick={handleMainClick}/>
+                        )}
+                        <LinkButton to="/" text="Logout" onClick={handleLogoutClick}/>
+                    </>
+                ) : (
+                    <>
+                        <LinkButton to="/login" text="Login" onClick={handleMainClick}/>
+
+                        {currentPath !== "/register" && (
+                            <LinkButton to="/register" text="Register" onClick={handleMainClick}/>
+                        )}
+                    </>
+                )}
+                {isAdmin && user ? <LinkButton to="/admin" text="List all users" state={{from: id}} />
+                    : ""}
+            </div>
+            <div className="buttonContainer">
+                <LinkButton to="/contact" text="Contact" onClick={handleContactClick}/>
+                <LinkButton to="/packages" text="Packages" onClick={handlePackagesClick}/>
+                {currentPath !== "/" && (
+                    <LinkButton to="/" text="Main Page" onClick={handleMainClick}/>
+                )}
+
             </div>
         </header>
     );
