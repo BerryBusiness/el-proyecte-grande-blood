9549fcee35b629f83111c1e0f4e12596afba7d56
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-05-16 12:03:34 +0200

    Move login and reg buttons to header, add images

--- frontend/my-angular-app/src/app/pages/main-page/main-page.component.ts
+++ frontend/my-angular-app/src/app/pages/main-page/main-page.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterModule } from '@angular/router';
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterOutlet,
-    RouterModule
-  ],
+    imports: [
+        RouterLink,
+        RouterOutlet,
+        RouterModule,
+        MatButton
+    ],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
