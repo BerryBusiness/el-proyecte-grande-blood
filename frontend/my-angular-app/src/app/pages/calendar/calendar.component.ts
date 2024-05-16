e8b0f4b1b6030ca7d126348e6aff567a60a33183
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-05-16 12:03:50 +0200

    Create calendar component

---
+++ frontend/my-angular-app/src/app/pages/calendar/calendar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import {CalendarOptions} from "@fullcalendar/core";
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [CommonModule, FullCalendarModule],
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent {
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    weekends: false,
+    events: [
+      { title: 'Meeting', start: new Date() }
+    ]
+  };
+}
