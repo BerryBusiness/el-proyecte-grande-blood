e1c4a85c03a11596b208092f369028b580b6fce6
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2024-05-16 12:02:37 +0200

    Creating a service to handle state

--- frontend/my-angular-app/src/app/services/auth.service.ts
+++ frontend/my-angular-app/src/app/services/auth.service.ts
@@ -1,20 +1,57 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AuthResponse } from './auth-response.interface'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/api'; // Adjust this to match your backend endpoint
+  private apiUrl = 'http://localhost:8080/api/users';
+  private loggedIn = new BehaviorSubject<boolean>(false);
+  private userName = new BehaviorSubject<string | null>(null);
 
   constructor(private http: HttpClient) {}
 
-  login(credentials: { username: string; password: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/users/login`, credentials);
+  get isLoggedIn(): Observable<boolean> {
+    return this.loggedIn.asObservable();
   }
 
-  register(data: { username: string; password: string; email: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/users/register`, data);
+  get currentUserName(): Observable<string | null> {
+    return this.userName.asObservable();
+  }
+
+  login(credentials: { username: string; password: string }): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credentials).pipe(
+      tap(response => {
+        if (response.token) {
+          this.loggedIn.next(true);
+          this.userName.next(credentials.username);
+          localStorage.setItem('authToken', response.token);
+        }
+      })
+    );
+  }
+
+
+  register(data: { username: string; password: string; email: string }): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/register`, data).pipe(
+      tap(response => {
+        if (response.token) {
+          this.loggedIn.next(true);
+          this.userName.next(data.username);
+          console.log(data.username);
+          localStorage.setItem('authToken', response.token);
+        }
+      })
+    );
+  }
+
+  logout(): void {
+    this.loggedIn.next(false);
+    this.userName.next(null);
+    localStorage.removeItem('authToken');
   }
 }
